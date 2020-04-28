50092ac6e17a540f8cd2619e0494d72adf5fb099
IvanPolienko <piar1986@yandex.ru>
2020-04-28 22:23:01 +0700

    Update manage.py

--- manage.py
+++ manage.py
@@ -34,39 +34,40 @@ if __name__ == '__main__':
     logger.addHandler(MyLogsHandler())
     logger.warning('Бот запущен')    
     
-    try:
-        0/0
     while True:
         try:
-            response = requests.get(url_template, headers=headers, timeout=91, params = {'timestamp': timestamp})
-            response.raise_for_status()
-            json_data = response.json()
-        
-            if json_data['status']=="found":
-                lesson_data = json_data['new_attempts'][0]
-                lesson_title = lesson_data['lesson_title']
-                lesson_url = "https://dvmn.org" + lesson_data['lesson_url']
-                lesson_result = lesson_data['is_negative']
-              
-                if lesson_result:
-                    lesson_comment = "К сожалению, в работе нашлись ошибки."
-                else:
-                    lesson_comment = "Преподавателю все понравилось, можно приступать к следующему уроку!"
-        
-                message_text = text.format(lesson_title, lesson_comment, lesson_url)
-                bot.send_message(chat_id = chat_id, text = message_text)
-                timestamp = json_data['last_attempt_timestamp']
-        
-            elif json_data['status']=="timeout":
-              timestamp = json_data['timestamp_to_request']
-        
-        except requests.exceptions.ReadTimeout:
-            pass
-        
-        except requests.ConnectionError:
-            connection_error_count += 1
-            if connection_error_count == 3:
-                sleep(60)
-                connection_error_count = 0
-    except Exception as err:
-        logger.error(err, exc_info=True)
+            0/0
+            while True:
+                try:
+                    response = requests.get(url_template, headers=headers, timeout=91, params = {'timestamp': timestamp})
+                    response.raise_for_status()
+                    json_data = response.json()
+                
+                    if json_data['status']=="found":
+                        lesson_data = json_data['new_attempts'][0]
+                        lesson_title = lesson_data['lesson_title']
+                        lesson_url = "https://dvmn.org" + lesson_data['lesson_url']
+                        lesson_result = lesson_data['is_negative']
+                      
+                        if lesson_result:
+                            lesson_comment = "К сожалению, в работе нашлись ошибки."
+                        else:
+                            lesson_comment = "Преподавателю все понравилось, можно приступать к следующему уроку!"
+                
+                        message_text = text.format(lesson_title, lesson_comment, lesson_url)
+                        bot.send_message(chat_id = chat_id, text = message_text)
+                        timestamp = json_data['last_attempt_timestamp']
+                
+                    elif json_data['status']=="timeout":
+                      timestamp = json_data['timestamp_to_request']
+                
+                except requests.exceptions.ReadTimeout:
+                    pass
+                
+                except requests.ConnectionError:
+                    connection_error_count += 1
+                    if connection_error_count == 3:
+                        sleep(60)
+                        connection_error_count = 0
+        except Exception as err:
+            logger.error(err, exc_info=True)
